0d1139202d214ca968c860e03508ba34fac96378
rusiang <ko8722375@gmail.com>
2019-07-07 22:06:36 +0800

    個股_計算現金發放率

--- individual/dividendTable.php
+++ individual/dividendTable.php
@@ -1,35 +1,48 @@
 <?php
-if (empty($_GET))
-    $_GET['company'] = '2330';
-$sql = "select * ".
-       "from dividend ".
-       "where code = '".$_GET['company']."' ".
-       "order by year desc ";
-$result = $conn->query($sql);
-$total_records = mysqli_num_rows($result);  // 取得記錄數
+  if (empty($_GET))
+      $_GET['company'] = '2330';
 
-/////////////////////////// 標題 /////////////////////////// 
-echo "\"<div class='table100 ver1' id='monthlyTbl'>".
-     "<table data-vertable='ver1'>".
-     "<thead>".
-       "<tr class='row100 head'>".
-         "<th>年度</th>".
-         "<th>現金股利</th>".
-         "<th>股票股利</th>".
-         "<th>總和</th>". 
-       "</tr>".
-     "</thead><tbody>";
+  $sql = "select d.*, ".
+         "       (case ".
+         "           when (d.cash = 0) then 0 ".
+         "           when (e.eps = 0) then 0 ".
+         "           else round(d.cash*100/e.eps,2) ".
+         "       end) dispatch ". 
+         "from dividend d, ".
+         "    (select year, eps ".
+         "    from eps ".
+         "    where code = '".$_GET['company']."' ".
+         "    and season = 'Q4')e ".
+         "where code = '".$_GET['company']."' ".
+         "and d.year = e.year ".
+         "order by year desc ";
+  $result = $conn->query($sql);
+  $total_records = mysqli_num_rows($result);  // 取得記錄數
 
-/////////////////////////// 欄位 /////////////////////////// 
-for ($i=0;$i<$total_records;$i++){ 
-    $row = mysqli_fetch_assoc($result); //將陣列以欄位名索引
-    echo "<tr class='row100'>";
-    echo "  <td>".$row['year']."</td>"; 
-    echo "  <td>".$row['cash']."</td>";
-    echo "  <td>".$row['allotment']."</td>";
-    echo "  <td>".$row['total']."</td>";
-    echo "</tr>";
-}
-echo "</tbody></table></div>\"";
-$result->close();
+  /////////////////////////// 標題 /////////////////////////// 
+  echo "\"<div class='table100 ver1' id='monthlyTbl'>".
+       "<table data-vertable='ver1'>".
+       "<thead>".
+         "<tr class='row100 head'>".
+           "<th>年度</th>".
+           "<th>現金股利</th>".
+           "<th>股票股利</th>".
+           "<th>總和</th>".
+           "<th>現金發放率</th>". 
+         "</tr>".
+       "</thead><tbody>";
+
+  /////////////////////////// 欄位 /////////////////////////// 
+  for ($i=0;$i<$total_records;$i++){ 
+      $row = mysqli_fetch_assoc($result); //將陣列以欄位名索引
+      echo "<tr class='row100'>";
+      echo "  <td>".$row['year']."</td>"; 
+      echo "  <td>".$row['cash']."</td>";
+      echo "  <td>".$row['allotment']."</td>";
+      echo "  <td>".$row['total']."</td>";
+      echo "  <td>".$row['dispatch']."%</td>";
+      echo "</tr>";
+  }
+  echo "</tbody></table></div>\"";
+  $result->close();
 ?>
