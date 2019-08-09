c326ac036229f7955d10554fd01c96c976546d7c
rusiang <ko8722375@gmail.com>
2019-08-09 14:53:01 +0800

    增加公司產業

--- individual/subTitle.php
+++ individual/subTitle.php
@@ -2,11 +2,11 @@
   if (empty($_GET))
       $_GET['company'] = '2330';
   /************************顯示公司名稱*************************/
-  $sql = "select company ".
+  $sql = "select company, code, grp ".
          "from company_map ".
          "where code = '".$_GET['company']."' ";
   $result = $conn->query($sql);
   $row = mysqli_fetch_assoc($result); 
-  echo "<h3>".$row['company']."</h3>";
+  echo "<h3>".$row['company']."(".$row['code'].")"." - ".$row['grp']."</h3>";
   /***********************************************************/
 ?>
