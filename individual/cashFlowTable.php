2f2c6d25fe978a7e3603b9259cf7d8addbf565ff
rusiang <ko8722375@gmail.com>
2020-01-05 15:49:21 +0800

    新增投資/融資/淨現金流

--- individual/cashFlowTable.php
+++ individual/cashFlowTable.php
@@ -3,6 +3,7 @@
   /* Date     Author   ChangeList
   /* --------------------------------------------------------------------------------  
   /* 20200101 rusiang  新增營業現金流 
+  /* 20200105 rusiang  新增投資/融資/淨現金流 
   /**********************************************************************************/ 
   if (empty($_GET))
       $_GET['company'] = '2330';
@@ -42,8 +43,10 @@
   /*『SQL』營業現金流                                                                      
   /*********************************************************************************/
   echo "【營業現金流】<br>";
-  $sql = "select c.year, c.season, c.v1 cash, i.value profit, round(c.v1/i.value*100,2) cash_rate ".
+  $sql = "select c.year, c.season, c.v1 cash, i.value profit, round(c.v1/i.value*100,2) cash_rate, ".
+         "       c2.v1 investCash, c3.v1 financing, (c.v1+c2.v1+c3.v1) sum ".
          "from ".
+//       營業現金流
          "( ".
          "  select * ".
          "  from cash_flow c ".
@@ -52,6 +55,24 @@
          "  and c.season = '".$tSeason."' ".
          "  and c.col_name = '營業活動之淨現金流入（流出）'".
          ") c, ".
+//       投資現金流
+         "( ".
+         "  select * ".
+         "  from cash_flow c ".
+         "  where 1=1 ".
+         "  and c.code = '".$_GET['company']."' ".
+         "  and c.season = '".$tSeason."' ".
+         "  and c.col_name = '投資活動之淨現金流入（流出）'".
+         ") c2, ".
+//       融資現金流
+         "( ".
+         "  select * ".
+         "  from cash_flow c ".
+         "  where 1=1 ".
+         "  and c.code = '".$_GET['company']."' ".
+         "  and c.season = '".$tSeason."' ".
+         "  and c.col_name = '籌資活動之淨現金流入（流出）'".
+         ") c3, ".
          "( ".
          "  select i.value, i.year ".
          "  from income_2 i ".
@@ -60,17 +81,23 @@
          "  and i.season = '".$tSeason."' ".
          "  and i.col_name = '本期淨利（淨損）' ".
          ") i ".
-         "where c.year = i.year ";
+         "where c.year = i.year ".
+         "and c2.year = i.year ".
+         "and c3.year = i.year ";
+
   $result = $conn->query($sql);
   $total_records = mysqli_num_rows($result);  // 取得記錄數
-  echo "<div class='table100 ver1' id='monthlyTbl' style='height:250px;'>";
+  echo "<div class='table100 ver1' id='monthlyTbl' style='height:350px;'>";
   echo "  <table data-vertable='ver1'>";
   echo "    <thead>";
   echo "      <tr class='row100 head'>";
   echo "        <th>年度</th>";
+  echo "        <th>投資現金流入</th>";
+  echo "        <th>融資現金流入</th>";
   echo "        <th>營業現金流入</th>";
   echo "        <th>本期淨利</th>";
   echo "        <th>營業現金佔淨利比率</th>";
+  echo "        <th>淨現金流</th>";
   echo "      </tr>";
   echo "    </thead>";
   echo "    <tbody>";  
@@ -79,9 +106,12 @@
       $row = mysqli_fetch_assoc($result); //將陣列以欄位名索引
       echo "<tr>";
       echo "  <td>".$row['year'].$row['season']."</td>";
+      echo "  <td>".$row['investCash']."</td>";
+      echo "  <td>".$row['financing']."</td>";
       echo "  <td>".$row['cash']."</td>";
       echo "  <td>".$row['profit']."</td>";
       echo "  <td>".$row['cash_rate']."</td>";
+      echo "  <td>".$row['sum']."</td>";
       echo "</tr>";
   }
   echo "    </tbody>"; 
