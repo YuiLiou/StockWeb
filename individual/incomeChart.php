8850045bf5a1dfe073669403c26bb7959183f76c
rusiang <ko8722375@gmail.com>
2020-02-08 13:59:12 +0800

    損益表加入季營收

--- individual/incomeChart.php
+++ individual/incomeChart.php
@@ -15,12 +15,14 @@
             var operatingRate = [];
             var beforeTaxRate = [];
             var afterTaxRate = [];
+            var income = [];
             for(var i in response) {
-                season.push(response[i]['year'] + response[i]['season']);
+                season.push(response[i]['season']);
                 grossRate.push(response[i]['grossRate']);
                 operatingRate.push(response[i]['operatingRate']);
                 beforeTaxRate.push(response[i]['beforeTaxRate']);
                 afterTaxRate.push(response[i]['afterTaxRate']);
+                income.push(response[i]['income']);
             }
             // draw graph
             var chartdata = {
@@ -33,7 +35,8 @@
                         borderColor: '#7441AA',
                         pointRadius: 10,
                         borderWidth: 5,
-                        data: grossRate
+                        data: grossRate,
+                        yAxisID: 'y-axis-1'
                     },{    
                         label: '營業利益率',                        
                         fill: false,                        
@@ -41,7 +44,8 @@
                         borderColor: '#99BD51',
                         pointRadius: 10,
                         borderWidth: 5,
-                        data: operatingRate
+                        data: operatingRate,
+                        yAxisID: 'y-axis-1'
                     },{    
                         label: '稅前淨利率',                        
                         fill: false,                        
@@ -49,7 +53,8 @@
                         borderColor: '#312B73',
                         pointRadius: 10,
                         borderWidth: 5,
-                        data: beforeTaxRate
+                        data: beforeTaxRate,
+                        yAxisID: 'y-axis-1'
                     },{    
                         label: '稅後淨利率',                        
                         fill: false,                        
@@ -57,8 +62,18 @@
                         borderColor: '#0D2611',
                         pointRadius: 10,
                         borderWidth: 5,
-                        data: afterTaxRate
-                    }
+                        data: afterTaxRate,
+                        yAxisID: 'y-axis-1'
+                    },{    
+                        label: '營收',                        
+                        fill: false,                        
+                        backgroundColor: '#ff6600',
+                        borderColor: '#ff6600',
+                        pointRadius: 10,
+                        borderWidth: 5,
+                        data: income,
+                        yAxisID: 'y-axis-2'
+                    }                     
                 ],	                 
             };
             var ctx = document.getElementById('myChart');
@@ -75,11 +90,20 @@
                     },
                     scales: {
                         yAxes: [{
-                            ticks: {
-                                fontColor: 'black',                               
-                                fontSize: '20'
+			    type: 'linear',
+                            display: true,
+                            position: 'left',
+                            id: 'y-axis-1',
+                        }, {
+			    type: 'linear',
+                            display: true,
+                            position: 'right',
+                            id: 'y-axis-2',
+                            // grid line settings
+                            gridLines: {
+                                drawOnChartArea: false, 
                             },
-                        }],
+                        }], 
                         xAxes: [{
                             ticks: {
                                 fontColor: 'black',
--- info/income.php
+++ info/income.php
@@ -1,21 +1,44 @@
 <?php
-// error message
-ini_set('display_errors','1');
-error_reporting(E_ALL);
+  /**********************************************************************************/
+  /* Date     Author   ChangeList
+  /* --------------------------------------------------------------------------------  
+  /* 20200208 rusiang  增加季營收
+  /**********************************************************************************/ 
+  // error message
+  ini_set('display_errors','1');
+  error_reporting(E_ALL);
 
-// connect to db
-require_once('db.php');
-$sql = "select * ".
-       "from income ".
-       "where code = '".$_GET['company']."' ".
-       "order by year asc, season asc";
-$result = $conn->query($sql);
-$data = array();
-foreach ($result as $row)
-{
-    $data[] = $row;
-}
-// close db
-$result->close();
-echo json_encode($data);
+  // connect to db
+  require_once('db.php');
+  if (empty($_GET)) $_GET['company'] = '2330';
+  $sql = "select a.grossRate, a.operatingRate, a.beforeTaxRate, a.afterTaxRate, ".
+         "       concat(a.year, a.season) season, ".
+         //      單季營收
+         "       ( ".
+         "         select sum(current) ".
+         "         from monthly ".
+         "         where 1=1 ".
+         "         and code = a.code ".
+         "         and month like concat(a.year,'%') ".
+         "         and ((a.season = 'Q4' and ".
+         "              (month like '____12' or month like '____11' or month like '____10')) or ".
+         "              (a.season = 'Q3' and ".
+         "              (month like '____09' or month like '____08' or month like '____07')) or ".
+         "              (a.season = 'Q2' and ".
+         "              (month like '____06' or month like '____05' or month like '____04')) or ".
+         "              (a.season = 'Q1' and ".
+         "             (month like '____03' or month like '____02' or month like '____01'))) ".
+         "       ) income ".
+         "from income a ".
+         "where code = '".$_GET['company']."' ".
+         "order by year asc, season asc"; 
+  $result = $conn->query($sql);
+  $data = array();
+  foreach ($result as $row)
+  {
+      $data[] = $row;
+  }
+  // close db
+  $result->close();
+  echo json_encode($data);
 ?>
